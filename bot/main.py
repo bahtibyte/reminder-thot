f5b42804353850ef7579d86102f9b5018b08bf1b
bahti <rasulovbahti@gmail.com>
2022-04-29 17:36:08 -0400

    changed the foundation

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,44 @@
-import os
+from pymongo import MongoClient
 from discord.ext import commands
+from quart import Quart
+from datetime import datetime
+import os
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+mongo = MongoClient(os.getenv('MONGO_HOST'))
+alerts = mongo['reminder-thot']['alerts']
+
+bot = commands.Bot(command_prefix='-')
+
+app = Quart(__name__)
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+    print(f'{bot.user} has connected to Discord!')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+@bot.command(name="hi")
+async def hi_command(ctx):
+    await ctx.channel.send("hello")
 
-if __name__ == "__main__":
-    bot.run(TOKEN)
+@bot.event
+async def on_message(message):
+    if message.content.startswith('-alert'):
+
+        msg = message.content.split('|')
+        
+        data = {
+            'title': msg[1].strip(),
+            'body': msg[2].strip(),
+            'date': msg[3].strip()
+        }
+        
+        alerts.insert_one(data)
+
+        await message.channel.send('d')
+
+@app.route("/")
+async def hello():
+    alerts.insert_one({'info': 'user visited', 'time': datetime.now()})
+    return "hello world by bahti"
+
+bot.loop.create_task(app.run_task())
+bot.run(os.getenv('DISCORD_TOKEN'))
