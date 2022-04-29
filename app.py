e4803810dd87d9846353ffba396d2cc923e050eb
bahti <rasulovbahti@gmail.com>
2022-04-29 14:57:04 -0400

    added hypercorn

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from quart import Quart
+import asyncio
+import logging
+import os
+import asyncio
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
 
-app = Flask(__name__)
+loop = asyncio.get_event_loop()
+app = Quart(__name__)
+
+log = logging.getLogger(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "Hello World! From BootcampV2"
+
+# loop.create_task(discord.connect())
+
+if os.getenv('ENV') == 'PROD':
+    port = os.getenv('PORT')
+    hypercorn_config = Config()
+    hypercorn_config.bind = ["0.0.0.0:" + port]
+
+    log.info(f"\n\nRunning on: {hypercorn_config.bind}\n\n")
+    loop.run_until_complete(serve(app, hypercorn_config))
+else:
+    loop.run_until_complete(serve(app, Config()))
+
+
+
